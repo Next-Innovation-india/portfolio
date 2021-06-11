08b53b7690834a9c6cf70ddd623a5e7ca33b8239
Aim Salve <79662667+Next-Innovation-india@users.noreply.github.com>
2021-06-11 16:38:24 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,345 @@
+
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+	<title>Apna Store Hai | About</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<meta name="keywords" content="Goggles a Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
+	<script>
+		addEventListener("load", function () {
+			setTimeout(hideURLbar, 0);
+		}, false);
+
+		function hideURLbar() {
+			window.scrollTo(0, 1);
+		}
+	</script>
+	<?php include 'links.php'; ?>
+</head>
+
+<body>
+	<div class="banner-top container-fluid" id="home">
+		<!-- header -->
+       <?php include 'header.php'; ?>
+		<!-- banner -->
+		<div class="banner_inner">
+			<div class="services-breadcrumb">
+				<div class="inner_breadcrumb">
+
+					<ul class="short">
+						<li>
+							<a href="index.html">Home</a>
+							<i>|</i>
+						</li>
+						<li>About Us</li>
+					</ul>
+				</div>
+			</div>
+
+		</div>
+		<!--//banner -->
+	</div>
+	<!--// header_top -->
+	<!-- top Products -->
+	<section class="banner-bottom-wthreelayouts py-lg-5 py-3">
+		<div class="container-fluid">
+
+			<div class="inner-sec-shop px-lg-4 px-3">
+				<div class="about-content py-lg-5 py-3">
+					<div class="row">
+
+						<div class="col-lg-6 p-0">
+							<img src="images/about.jpg" alt="Goggles" class="img-fluid">
+						</div>
+						<div class="col-lg-6 about-info">
+							<h3 class="tittle-w3layouts text-left mb-lg-5 mb-3">About Us</h3>
+							<p class="my-xl-4 my-lg-3 my-md-4 my-3">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+								dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
+								book.
+							</p>
+
+							<a href="shop.html" class="btn btn-sm animated-button gibson-three mt-4">Shop Now</a>
+
+						</div>
+					</div>
+				</div>
+				<h3 class="tittle-w3layouts text-center my-lg-4 my-4">Our Board of Directors</h3>
+				<div class="partners-info">
+					<div class="row mt-lg-5 mt-3">
+						<div class="col-md-3 team-main-gd">
+							<div class="team-grid text-center">
+								<div class="team-img">
+									<img class="img-fluid rounded" src="images/team1.jpg" alt="">
+								</div>
+								<div class="team-info">
+									<h4>Mr. Amit khan</h4>
+									
+									<ul class="d-flex justify-content-center py-3 social-icons">
+										<li>
+											<a href="#">
+												<i class="fab fa-facebook-f"></i>
+											</a>
+										</li>
+										<li class="mx-3">
+											<a href="#">
+												<i class="fab fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i class="fab fa-google-plus-g"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					 <div class="col-md-3 team-main-gd">
+							<div class="team-grid text-center">
+								<div class="team-img">
+									<img class="img-fluid rounded" src="images/team2.jpg" alt="">
+								</div>
+								<div class="team-info">
+									<h4>Mrs. Reshmaa Tiwari</h4>
+									
+									<ul class="d-flex justify-content-center py-3 social-icons">
+										<li>
+											<a href="#">
+												<i class="fab fa-facebook-f"></i>
+											</a>
+										</li>
+										<li class="mx-3">
+											<a href="#">
+												<i class="fab fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i class="fab fa-google-plus-g"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 team-main-gd">
+							<div class="team-grid text-center">
+								<div class="team-img">
+									<img class="img-fluid rounded" src="images/team3.jpg" alt="">
+								</div>
+								<div class="team-info">
+									<h4>Mr. Anuj Rathor</h4>
+									
+									<ul class="d-flex justify-content-center py-3 social-icons">
+										<li>
+											<a href="#">
+												<i class="fab fa-facebook-f"></i>
+											</a>
+										</li>
+										<li class="mx-3">
+											<a href="#">
+												<i class="fab fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i class="fab fa-google-plus-g"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 team-main-gd">
+							<div class="team-grid text-center">
+								<div class="team-img">
+									<img class="img-fluid rounded" src="images/team4.jpg" alt="">
+								</div>
+								<div class="team-info">
+									<h4>Ms. Mansi Singh</h4>
+									
+									<ul class="d-flex justify-content-center py-3 social-icons">
+										<li>
+											<a href="#">
+												<i class="fab fa-facebook-f"></i>
+											</a>
+										</li>
+										<li class="mx-3">
+											<a href="#">
+												<i class="fab fa-twitter"></i>
+											</a>
+										</li>
+										<li>
+											<a href="#">
+												<i class="fab fa-google-plus-g"></i>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- /grids -->
+				<?php include 'grids.php'; ?>
+				<!-- //grids -->
+				<!--/meddle-->
+				<div class="row">
+					<div class="col-md-12 middle-slider my-4">
+						<div class="middle-text-info ">
+
+							<h3 class="tittle-w3layouts two text-center my-lg-4 mt-3">Offer ends in</h3>
+							<div class="simply-countdown-custom" id="simply-countdown-custom"></div>
+
+						</div>
+					</div>
+				</div>
+				<!--//meddle-->
+
+				<!-- /testimonials -->
+				<?php include 'testinmonials.php'; ?>
+				<!-- //testimonials -->
+				<div class="row galsses-grids pt-lg-5 pt-3">
+					<div class="col-lg-6 galsses-grid-left">
+						<figure class="effect-lexi">
+							<img src="images/banner4.jpg" alt="" class="img-fluid">
+							<figcaption>
+								<h3>Editor's
+									<span>Pick</span>
+								</h3>
+								<p> Express your style now.</p>
+							</figcaption>
+						</figure>
+					</div>
+					<div class="col-lg-6 galsses-grid-left">
+						<figure class="effect-lexi">
+							<img src="images/banner1.jpg" alt="" class="img-fluid">
+							<figcaption>
+								<h3>Editor's
+									<span>Pick</span>
+								</h3>
+								<p>Express your style now.</p>
+							</figcaption>
+						</figure>
+					</div>
+				</div>
+
+				<!-- /clients-sec -->
+				<?php include 'clientsection.php'; ?>
+				<!-- //clients-sec -->
+
+			</div>
+		</div>
+	</section>
+
+
+	<!--footer -->
+	<?php include 'footer.php'; ?>
+	<!-- //footer -->
+
+	<!--jQuery-->
+	<script src="js/jquery-2.2.3.min.js"></script>
+	<!-- newsletter modal -->
+	<!--search jQuery-->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<script src="js/classie-search.js"></script>
+	<script src="js/demo1-search.js"></script>
+	<!--//search jQuery-->
+	<!-- cart-js -->
+	<script src="js/minicart.js"></script>
+	<script>
+		googles.render();
+
+		googles.cart.on('googles_checkout', function (evt) {
+			var items, len, i;
+
+			if (this.subtotal() > 0) {
+				items = this.items();
+
+				for (i = 0, len = items.length; i < len; i++) {}
+			}
+		});
+	</script>
+	<!-- //cart-js -->
+	<script>
+		$(document).ready(function () {
+			$(".button-log a").click(function () {
+				$(".overlay-login").fadeToggle(200);
+				$(this).toggleClass('btn-open').toggleClass('btn-close');
+			});
+		});
+		$('.overlay-close1').on('click', function () {
+			$(".overlay-login").fadeToggle(200);
+			$(".button-log a").toggleClass('btn-open').toggleClass('btn-close');
+			open = false;
+		});
+	</script>
+	<!-- carousel -->
+	<!-- dropdown nav -->
+	<script>
+		$(document).ready(function () {
+			$(".dropdown").hover(
+				function () {
+					$('.dropdown-menu', this).stop(true, true).slideDown("fast");
+					$(this).toggleClass('open');
+				},
+				function () {
+					$('.dropdown-menu', this).stop(true, true).slideUp("fast");
+					$(this).toggleClass('open');
+				}
+			);
+		});
+	</script>
+	<!-- //dropdown nav -->
+	<!-- Count-down -->
+	<script src="js/simplyCountdown.js"></script>
+	<link href="css/simplyCountdown.css" rel='stylesheet' type='text/css' />
+	<script>
+		var d = new Date();
+		simplyCountdown('simply-countdown-custom', {
+			year: d.getFullYear(),
+			month: d.getMonth() + 2,
+			day: 25
+		});
+	</script>
+	<!--// Count-down -->
+	<script src="js/move-top.js"></script>
+    <script src="js/easing.js"></script>
+    <script>
+        jQuery(document).ready(function($) {
+            $(".scroll").click(function(event) {
+                event.preventDefault();
+                $('html,body').animate({
+                    scrollTop: $(this.hash).offset().top
+                }, 900);
+            });
+        });
+    </script>
+    <script>
+        $(document).ready(function() {
+            /*
+            						var defaults = {
+            							  containerID: 'toTop', // fading element id
+            							containerHoverID: 'toTopHover', // fading element hover id
+            							scrollSpeed: 1200,
+            							easingType: 'linear' 
+            						 };
+            						*/
+
+            $().UItoTop({
+                easingType: 'easeOutQuart'
+            });
+
+        });
+    </script>
+    <!--// end-smoth-scrolling -->
+
+
+	<script src="js/bootstrap.js"></script>
+	<!-- js file -->
+</body>
+
+</html>
